98bb7d0e0698eea17a415a14a08d962565f2abf7
Unknown <lauri.askonen@gmail.com>
2018-09-15 12:19:48 +0300

    form.js luotu
    Funktiot saatu toimimaan

---
+++ form.js
@@ -0,0 +1,10 @@
+function showLanguage() {
+  var language = document.getElementById('language').value;
+  var selectedLanguage = language
+  document.getElementById('selected_language').value = selectedLanguage;
+
+}
+
+function showPhone(phone) {
+  document.getElementById('selected_phone').value = phone;
+}
